22ce413d998d0364435f3e23a2bb658aa42c97ca
rafeldev <tecnoportuaria@gmail.com>
2021-04-02 13:51:39 -0500

    header listo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
 
+/* Icons */
+import arrowBack from './assets/icons/left-arrow.svg'
+import noun_notification from './assets/icons/noun_notification.png'
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img  className="App-logo" alt="logo" />
-        <p>
-          Hola Be-Go
-        </p>
+        <img width="14px" color="white" src={arrowBack} alt="arrowBack" />
+        <h1>Tasks</h1>
+        <img src={noun_notification} alt="campaign" />
       </header>
+      <div className="App-button">
+        <button className="button">CREATE +</button>
+      </div>
     </div>
   );
 }
